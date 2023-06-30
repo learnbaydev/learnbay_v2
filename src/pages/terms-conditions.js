a9182500bd4daae4e2253858d34114c85343b57b
Mohd-w123 <codewithwazid@gmail.com>
2023-06-30 15:33:03 +0530

    pages added

---
+++ src/pages/terms-conditions.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Head from "next/head";
+import Terms from "../../components/WebPage/Terms/Terms";
+import Navbar from "../../components/Navbar/Navbar";
+import OfferPopup from "../../components/OfferPopup/OfferPopup";
+import Footer from "../../components/Footer/Footer";
+
+function terms() {
+  return (
+    <div>
+      <Head>
+        <title>Learnbay - Terms and Conditions</title>
+        <meta name="robots" content="index, follow" />
+        <meta name="description" content="Learnbay's terms and conditions are clear and concise for your peace of mind. Explore more information for a smooth learning experience. Come join us today!" />
+        <link
+          rel="icon"
+          href="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/Learnbay-Favicon-L.png"
+        />
+        <link rel="canonical" href="https://www.learnbay.co/terms-conditions" />
+      </Head>
+      <main>
+        <Navbar popup={true} dataScienceCounselling={true} radio={true} />
+        <Terms />
+
+        <OfferPopup offer={false} />
+        <Footer />
+      </main>
+    </div>
+  );
+}
+
+export default terms;
