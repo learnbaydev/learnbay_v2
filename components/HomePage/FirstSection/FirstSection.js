d232ebd4dfba49017fffc614ccc07dcac7ae8c99
ammar-tanwar <ammartanwar123@gmail.com>
2023-07-20 18:24:47 +0530

    new home section

--- components/HomePage/FirstSection/FirstSection.js
+++ components/HomePage/FirstSection/FirstSection.js
@@ -7,6 +7,12 @@ import Image from "next/image";
 import Typed from "typed.js";
 import dynamic from "next/dynamic";
 import { Content } from "next/font/google";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 const Button = dynamic(() => import("../../Global/Button/Button"));
 
 const FirstSection = ({
@@ -16,6 +22,8 @@ const FirstSection = ({
   dataScienceCounselling,
 }) => {
   const [popups, setPopups] = useState(false);
+  const [mobile, setMobile] = useState(false);
+
   const el = useRef(null);
 
   const popupShow = () => {
@@ -46,111 +54,195 @@ const FirstSection = ({
 
   return (
     <>
-      <div className={styles.First}>
-        <Popup
-          trigger={popups}
-          setTrigger={setPopups}
-          className="popupModal"
-          popup={true}
-          radio={radio}
-          dataScience={dataScience}
-          dataScienceCounselling={dataScienceCounselling}
-        >
-          <div className="leftPopup">
-            <div
-              className="whiteP"
-              style={{ width: "340px", height: "400px" }}
-            ></div>
-          </div>
-          <div className="RightPopup">
-            <h5>Apply For Counselling</h5>
-            <Form
-              dataScience={dataScience}
-              dataScienceCounselling={dataScienceCounselling}
-              radio={radio}
-            />
-          </div>
-        </Popup>
-        <div className="bgWrap">
-          <Image
-            src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/FirstSectionBg.png"
-            fill={true}
-            priority={true}
-            alt="background Image"
-            style={{ objectFit: "contain" }}
+      <Popup
+        trigger={popups}
+        setTrigger={setPopups}
+        className="popupModal"
+        popup={true}
+        radio={radio}
+        dataScience={dataScience}
+        dataScienceCounselling={dataScienceCounselling}
+      >
+        <div className="leftPopup">
+          <div
+            className="whiteP"
+            style={{ width: "340px", height: "400px" }}
+          ></div>
+        </div>
+        <div className="RightPopup">
+          <h5>Apply For Counselling</h5>
+          <Form
+            dataScience={dataScience}
+            dataScienceCounselling={dataScienceCounselling}
+            radio={radio}
           />
         </div>
-        <div className={styles.FirstLeft}>
-          <div className={styles.animationTextWrap}>
-            <span ref={el} className={styles.animationText}></span>
-          </div>
-          {/* <p className={styles.ptop}>
+      </Popup>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={mobile ? 0 : 0}
+        pagination={{
+          clickable: true,
+        }}
+        grabCursor={true}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide className={styles.slide}>
+          <div className={styles.First}>
+            <div className="bgWrap">
+              <Image
+                src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/FirstSectionBg.png"
+                fill={true}
+                priority={true}
+                alt="background Image"
+                style={{ objectFit: "contain" }}
+              />
+            </div>
+            <div className={styles.FirstLeft}>
+              <div className={styles.animationTextWrap}>
+                <span ref={el} className={styles.animationText}></span>
+              </div>
+              {/* <p className={styles.ptop}>
             Take the first step in your blockchain development journey with our
           </p> */}
-          <h1 className={styles.h1}>
-            Domain Specialized Certification Program{" "}
-            <span className={styles.h1Span}>For Working Professionals</span>
-          </h1>
-          <div className={styles.imgWrapperMobile}>
-            <Image
-              src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/Home-Page-Header.png"
-              width="580"
-              height="450"
-              alt="data science course"
-            />
-          </div>
-          <p className={styles.ptop}>In collaboration with</p>
-          <div className={styles.ImageBlock}>
-            <div className="imgWrapper">
-              <Image
-                src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/FirstSection.png"
-                width="283"
-                height="51"
-                priority={true}
-                alt="data science course"
-              />
+              <h1 className={styles.h1}>
+                Domain Specialized Certification Program{" "}
+                <span className={styles.h1Span}>For Working Professionals</span>
+              </h1>
+              <div className={styles.imgWrapperMobile}>
+                <Image
+                  src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/Home-Page-Header.png"
+                  width="580"
+                  height="450"
+                  alt="data science course"
+                />
+              </div>
+              <p className={styles.ptop}>In collaboration with</p>
+              <div className={styles.ImageBlock}>
+                <div className="imgWrapper">
+                  <Image
+                    src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/FirstSection.png"
+                    width="283"
+                    height="51"
+                    priority={true}
+                    alt="data science course"
+                  />
+                </div>
+              </div>
+              <p className={styles.blink}>
+                Advance your career by gaining expertise in your field and ace
+                interviews with India’s leading companies
+              </p>
+              <div className={styles.btnWrapper}>
+                <a href="#course">
+                  <Button
+                    className={styles.outLineBtn}
+                    outline
+                    text="Courses"
+                    passIcon={<FaChevronDown className="bIconS" />}
+                  />
+                </a>
+                <div onClick={popupShow}>
+                  <Button
+                    className={styles.Btn}
+                    text="Enquire Now"
+                    passIcon={<FaBell className="bIconS" />}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-          <p className={styles.blink}>
-            Advance your career by gaining expertise in your field and ace
-            interviews with India’s leading companies
-          </p>
-          {/* <div className={styles.line}>
-            <img
-              src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/Vector-1-line.png"
-              width="80px"
-            />
-          </div> */}
-          <div className={styles.btnWrapper}>
-            <a href="#course">
-              <Button
-                className={styles.outLineBtn}
-                outline
-                text="Courses"
-                passIcon={<FaChevronDown className="bIconS" />}
-              />
-            </a>
-            <div onClick={popupShow}>
-              <Button
-                className={styles.Btn}
-                text="Enquire Now"
-                passIcon={<FaBell className="bIconS" />}
-              />
-            </div>
-          </div>
-        </div>
 
-        <div className={styles.secondLeft}>
-          <div className={styles.imgWrapperDesktop}>
-            <Image
-              src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/Home-Page-Header.png"
-              width="580"
-              height="450"
-              alt="data science course"
-            />
+            <div className={styles.secondLeft}>
+              <div className={styles.imgWrapperDesktop}>
+                <Image
+                  src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/Home-Page-Header.png"
+                  width="580"
+                  height="450"
+                  alt="data science course"
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </SwiperSlide>
+        <SwiperSlide className={styles.slide}>
+          <div className={styles.First}>
+            <div className="bgWrap">
+              <Image
+                src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/FirstSectionBg.png"
+                fill={true}
+                priority={true}
+                alt="background Image"
+                style={{ objectFit: "contain" }}
+              />
+            </div>
+            <div className={styles.FirstLeft}>
+              <div className={styles.animationTextWrap}>
+                <span ref={el} className={styles.animationText}></span>
+              </div>
+              {/* <p className={styles.ptop}>
+            Take the first step in your blockchain development journey with our
+          </p> */}
+              <h1 className={styles.h1}>
+                Domain Specialized Certification Program{" "}
+                <span className={styles.h1Span}>For Working Professionals</span>
+              </h1>
+              <div className={styles.imgWrapperMobile}>
+                <Image
+                  src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/Home-Page-Header.png"
+                  width="580"
+                  height="450"
+                  alt="data science course"
+                />
+              </div>
+              <p className={styles.ptop}>In collaboration with</p>
+              <div className={styles.ImageBlock}>
+                <div className="imgWrapper">
+                  <Image
+                    src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/FirstSection.png"
+                    width="283"
+                    height="51"
+                    priority={true}
+                    alt="data science course"
+                  />
+                </div>
+              </div>
+              <p className={styles.blink}>
+                Advance your career by gaining expertise in your field and ace
+                interviews with India’s leading companies
+              </p>
+              <div className={styles.btnWrapper}>
+                <a href="#course">
+                  <Button
+                    className={styles.outLineBtn}
+                    outline
+                    text="Courses"
+                    passIcon={<FaChevronDown className="bIconS" />}
+                  />
+                </a>
+                <div onClick={popupShow}>
+                  <Button
+                    className={styles.Btn}
+                    text="Enquire Now"
+                    passIcon={<FaBell className="bIconS" />}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.secondLeft}>
+              <div className={styles.imgWrapperDesktop}>
+                <Image
+                  src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/Home-Page-Header.png"
+                  width="580"
+                  height="450"
+                  alt="data science course"
+                />
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </>
   );
 };
