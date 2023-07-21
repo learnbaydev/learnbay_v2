14145cad6fae1fe6dcd18c4d2c371efe7bd54e4c
Mohd-w123 <codewithwazid@gmail.com>
2023-07-21 17:17:35 +0530

    format set for event

--- src/pages/event/Thank-You-event.js
+++ src/pages/event/Thank-You-event.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../../styles/ThankYou.module.css";
 import Head from "next/head";
 import Image from "next/image";
-import Navbar from "../../Components/Navbar/Navbar";
+import Navbar from "../../../components/Navbar/Navbar";
 
 const ThankYou = () => {
   return (
