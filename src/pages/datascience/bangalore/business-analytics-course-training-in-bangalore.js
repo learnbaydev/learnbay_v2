9834f38e28c84fdbcc667a3fa2365d2ddaa317a0
ammar-tanwar <ammartanwar123@gmail.com>
2023-07-25 12:36:36 +0530

    new city pages of pune, dubai, and bangalore

--- src/pages/datascience/bangalore/business-analytics-course-training-in-bangalore.js
+++ src/pages/datascience/bangalore/business-analytics-course-training-in-bangalore.js
@@ -20,12 +20,12 @@ const CityText = dynamic(() =>
   import("../../../../components/SEO/CityText/CityText")
 );
 const CitiesLeft = dynamic(() =>
-  import("../../../../components/SEO/CitiesLeft/CitiesLeft")
+  import("../../../../components/SEO/CitiesLeft/CitiesLeftDomain")
 );
 const CitiesRight = dynamic(() =>
-  import("../../../../components/SEO/CitiesRight/CitiesRight")
+  import("../../../../components/SEO/CitiesRight/CitiesRightdomain")
 );
-import { DataScienceCourseData } from "../../../../CityData/Bangalore/artificialIntelligenceAiCourseTrainingBangaloreData";
+import { BABangaloreCourseData } from "../../../../CityData/Bangalore/businessAnalyticsCourseTrainingBangaloreData";
 const FAQNew = dynamic(() =>
   import("../../../../components/CoursePage/FAQNew/FAQNew")
 );
@@ -79,25 +79,25 @@ export default function Home() {
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: `${DataScienceCourseData[0].script1}`,
+            __html: `${BABangaloreCourseData[0].script1}`,
           }}
         />{" "}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: ` ${DataScienceCourseData[0].script2}`,
+            __html: ` ${BABangaloreCourseData[0].script2}`,
           }}
         />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: ` ${DataScienceCourseData[0].script3} `,
+            __html: ` ${BABangaloreCourseData[0].script3} `,
           }}
         />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: ` ${DataScienceCourseData[0].script4} `,
+            __html: ` ${BABangaloreCourseData[0].script4} `,
           }}
         />
       </Head>
@@ -123,14 +123,14 @@ export default function Home() {
           idss="4j80WXLTvGE"
           FirstRightImg="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/NewDesignImage/manager+header+image.webp"
           firstToparaImg="Training In Bangalore"
-          firstHeading="Business Analytics Master Program"
+          firstHeading="Business Analytics Course"
           firstTopPara="Job-ready Business Analytics Program with Live Industrial Projects"
           cityParaCont="The best-suited course for working professionals with less than a year of work experience and dreaming for a lucrative and even growing future."
         />
-        <SecondSection SecondSectionData={DataScienceCourseData[0].secondSection} />
+        <SecondSection SecondSectionData={BABangaloreCourseData[0].secondSection} />
         <Testimonial
           redirectDS={true}
-          Testimonial={DataScienceCourseData[0].testimonial}
+          Testimonial={BABangaloreCourseData[0].testimonial}
           heading="Our Alumni Speak"
           para="Discover the impact of our programs on career growth"
         />
@@ -153,137 +153,137 @@ export default function Home() {
           </div>
           <div className="CitiesRight">
             <CitiesRight
-              ProgramBot1={DataScienceCourseData[0].ProgramBot1}
-              ProgramHead3={DataScienceCourseData[0].ProgramHead3}
-              ProgramBot3={DataScienceCourseData[0].ProgramBot3}
-              ProgramBot31={DataScienceCourseData[0].ProgramBot31}
-              ProgramBot4={DataScienceCourseData[0].ProgramBot4}
-              ProgramBot5={DataScienceCourseData[0].ProgramBot5}
-              ProgramBot6={DataScienceCourseData[0].ProgramBot6}
-              Courselink1={DataScienceCourseData[0].Courselink1}
-              Courselink2={DataScienceCourseData[0].Courselink2}
-              Courselink3={DataScienceCourseData[0].Courselink3}
-              Courselink4={DataScienceCourseData[0].Courselink4}
-              Courselink5={DataScienceCourseData[0].Courselink5}
-              Course1={DataScienceCourseData[0].Course1}
-              Course2={DataScienceCourseData[0].Course2}
-              Course3={DataScienceCourseData[0].Course3}
-              Course4={DataScienceCourseData[0].Course4}
-              Course5={DataScienceCourseData[0].Course5}
-              DomainHead1={DataScienceCourseData[0].DomainHead1}
-              DomainBot1={DataScienceCourseData[0].DomainBot1}
-              DomainBot2={DataScienceCourseData[0].DomainBot2}
-              DomainBot3={DataScienceCourseData[0].DomainBot3}
-              DomainList1={DataScienceCourseData[0].DomainList1}
-              DomainList2={DataScienceCourseData[0].DomainList2}
-              DomainList3={DataScienceCourseData[0].DomainList3}
-              DomainList4={DataScienceCourseData[0].DomainList4}
-              DomainList5={DataScienceCourseData[0].DomainList5}
-              DomainList6={DataScienceCourseData[0].DomainList6}
-              DomainList7={DataScienceCourseData[0].DomainList7}
-              DomainList8={DataScienceCourseData[0].DomainList8}
-              DomainList9={DataScienceCourseData[0].DomainList9}
-              DomainList10={DataScienceCourseData[0].DomainList10}
-              Domainlink1={DataScienceCourseData[0].Domainlink1}
-              Domainlink2={DataScienceCourseData[0].Domainlink2}
-              Domainlink3={DataScienceCourseData[0].Domainlink3}
-              Domainlink4={DataScienceCourseData[0].Domainlink4}
-              Domainlink5={DataScienceCourseData[0].Domainlink5}
-              Domainlink6={DataScienceCourseData[0].Domainlink6}
-              Domainlink7={DataScienceCourseData[0].Domainlink7}
-              Domainlink8={DataScienceCourseData[0].Domainlink8}
-              Domainlink9={DataScienceCourseData[0].Domainlink9}
-              Domainlink10={DataScienceCourseData[0].Domainlink10}
-              CertificationBot1={DataScienceCourseData[0].CertificationBot1}
-              src={DataScienceCourseData[0].src}
-              src22={DataScienceCourseData[0].src22}
-              src33={DataScienceCourseData[0].src33}
-              ModuleBot1={DataScienceCourseData[0].ModuleBot1}
-              ModuleBot2={DataScienceCourseData[0].ModuleBot2}
-              syllabush1={DataScienceCourseData[0].syllabush1}
-              syllabusb1={DataScienceCourseData[0].syllabusb1}
-              SyllabusBotlist11={DataScienceCourseData[0].SyllabusBotlist11}
-              SyllabusBotlist12={DataScienceCourseData[0].SyllabusBotlist12}
-              SyllabusBotlist13={DataScienceCourseData[0].SyllabusBotlist13}
-              SyllabusBotlist14={DataScienceCourseData[0].SyllabusBotlist14}
-              SyllabusBotlist15={DataScienceCourseData[0].SyllabusBotlist15}
-              SyllabusBotlist16={DataScienceCourseData[0].SyllabusBotlist16}
-              syllabush2={DataScienceCourseData[0].syllabush2}
-              syllabusb2={DataScienceCourseData[0].syllabusb2}
-              SyllabusBotlist21={DataScienceCourseData[0].SyllabusBotlist21}
-              SyllabusBotlist22={DataScienceCourseData[0].SyllabusBotlist22}
-              SyllabusBotlist23={DataScienceCourseData[0].SyllabusBotlist23}
-              SyllabusBotlist24={DataScienceCourseData[0].SyllabusBotlist24}
-              SyllabusBotlist25={DataScienceCourseData[0].SyllabusBotlist25}
-              SyllabusBotlist26={DataScienceCourseData[0].SyllabusBotlist26}
-              syllabush3={DataScienceCourseData[0].syllabush3}
-              syllabusb3={DataScienceCourseData[0].syllabusb3}
-              SyllabusBotlist31={DataScienceCourseData[0].SyllabusBotlist31}
-              SyllabusBotlist32={DataScienceCourseData[0].SyllabusBotlist32}
-              SyllabusBotlist33={DataScienceCourseData[0].SyllabusBotlist33}
-              SyllabusBotlist34={DataScienceCourseData[0].SyllabusBotlist34}
-              SyllabusBotlist35={DataScienceCourseData[0].SyllabusBotlist35}
-              SyllabusBotlist36={DataScienceCourseData[0].SyllabusBotlist36}
-              syllabush4={DataScienceCourseData[0].syllabush4}
-              syllabusb4={DataScienceCourseData[0].syllabusb4}
-              SyllabusBotlist41={DataScienceCourseData[0].SyllabusBotlist41}
-              SyllabusBotlist42={DataScienceCourseData[0].SyllabusBotlist42}
-              SyllabusBotlist43={DataScienceCourseData[0].SyllabusBotlist43}
-              SyllabusBotlist44={DataScienceCourseData[0].SyllabusBotlist44}
-              SyllabusBotlist45={DataScienceCourseData[0].SyllabusBotlist45}
-              SyllabusBotlist46={DataScienceCourseData[0].SyllabusBotlist46}
-              syllabussrc={DataScienceCourseData[0].syllabussrc}
-              JobBot1={DataScienceCourseData[0].JobBot1}
-              JobHead2={DataScienceCourseData[0].JobHead2}
-              JobBot3={DataScienceCourseData[0].JobBot3}
-              JobBot4={DataScienceCourseData[0].JobBot4}
-              ProjectsBot1={DataScienceCourseData[0].ProgramBot1}
-              src1={DataScienceCourseData[0].src1}
-              ProjectsH1={DataScienceCourseData[0].ProjectsH1}
-              Projectsp1={DataScienceCourseData[0].Projectsp1}
-              src2={DataScienceCourseData[0].src2}
-              ProjectsH2={DataScienceCourseData[0].ProjectsH2}
-              Projectsp2={DataScienceCourseData[0].Projectsp2}
-              src3={DataScienceCourseData[0].src3}
-              ProjectsH3={DataScienceCourseData[0].ProjectsH3}
-              Projectsp3={DataScienceCourseData[0].Projectsp3}
-              src4={DataScienceCourseData[0].src4}
-              ProjectsH4={DataScienceCourseData[0].ProjectsH4}
-              Projectsp4={DataScienceCourseData[0].Projectsp4}
-              AlumniBot1={DataScienceCourseData[0].AlumniBot1}
-              Asrc1={DataScienceCourseData[0].Asrc1}
-              AlumniH1={DataScienceCourseData[0].AlumniH1}
-              Alumnip1={DataScienceCourseData[0].Alumnip1}
-              Asrc2={DataScienceCourseData[0].Asrc2}
-              AlumniH2={DataScienceCourseData[0].AlumniH2}
-              Alumnip2={DataScienceCourseData[0].Alumnip2}
-              Asrc3={DataScienceCourseData[0].Asrc3}
-              AlumniH3={DataScienceCourseData[0].AlumniH3}
-              Alumnip3={DataScienceCourseData[0].Alumnip3}
-              Asrc4={DataScienceCourseData[0].Asrc4}
-              AlumniH4={DataScienceCourseData[0].AlumniH4}
-              Alumnip4={DataScienceCourseData[0].Alumnip4}
-              alt1={DataScienceCourseData[0].alt1}
-              alt2={DataScienceCourseData[0].alt2}
-              alt3={DataScienceCourseData[0].alt3}
-              alt4={DataScienceCourseData[0].alt4}
-              alt5={DataScienceCourseData[0].alt5}
-              alt6={DataScienceCourseData[0].alt6}
-              alt7={DataScienceCourseData[0].alt7}
-              alt8={DataScienceCourseData[0].alt8}
-              alt9={DataScienceCourseData[0].alt9}
+              ProgramBot1={BABangaloreCourseData[0].ProgramBot1}
+              ProgramHead3={BABangaloreCourseData[0].ProgramHead3}
+              ProgramBot3={BABangaloreCourseData[0].ProgramBot3}
+              ProgramBot31={BABangaloreCourseData[0].ProgramBot31}
+              ProgramBot4={BABangaloreCourseData[0].ProgramBot4}
+              ProgramBot5={BABangaloreCourseData[0].ProgramBot5}
+              ProgramBot6={BABangaloreCourseData[0].ProgramBot6}
+              Courselink1={BABangaloreCourseData[0].Courselink1}
+              Courselink2={BABangaloreCourseData[0].Courselink2}
+              Courselink3={BABangaloreCourseData[0].Courselink3}
+              Courselink4={BABangaloreCourseData[0].Courselink4}
+              Courselink5={BABangaloreCourseData[0].Courselink5}
+              Course1={BABangaloreCourseData[0].Course1}
+              Course2={BABangaloreCourseData[0].Course2}
+              Course3={BABangaloreCourseData[0].Course3}
+              Course4={BABangaloreCourseData[0].Course4}
+              Course5={BABangaloreCourseData[0].Course5}
+              DomainHead1={BABangaloreCourseData[0].DomainHead1}
+              DomainBot1={BABangaloreCourseData[0].DomainBot1}
+              DomainBot2={BABangaloreCourseData[0].DomainBot2}
+              DomainBot3={BABangaloreCourseData[0].DomainBot3}
+              DomainList1={BABangaloreCourseData[0].DomainList1}
+              DomainList2={BABangaloreCourseData[0].DomainList2}
+              DomainList3={BABangaloreCourseData[0].DomainList3}
+              DomainList4={BABangaloreCourseData[0].DomainList4}
+              DomainList5={BABangaloreCourseData[0].DomainList5}
+              DomainList6={BABangaloreCourseData[0].DomainList6}
+              DomainList7={BABangaloreCourseData[0].DomainList7}
+              DomainList8={BABangaloreCourseData[0].DomainList8}
+              DomainList9={BABangaloreCourseData[0].DomainList9}
+              DomainList10={BABangaloreCourseData[0].DomainList10}
+              Domainlink1={BABangaloreCourseData[0].Domainlink1}
+              Domainlink2={BABangaloreCourseData[0].Domainlink2}
+              Domainlink3={BABangaloreCourseData[0].Domainlink3}
+              Domainlink4={BABangaloreCourseData[0].Domainlink4}
+              Domainlink5={BABangaloreCourseData[0].Domainlink5}
+              Domainlink6={BABangaloreCourseData[0].Domainlink6}
+              Domainlink7={BABangaloreCourseData[0].Domainlink7}
+              Domainlink8={BABangaloreCourseData[0].Domainlink8}
+              Domainlink9={BABangaloreCourseData[0].Domainlink9}
+              Domainlink10={BABangaloreCourseData[0].Domainlink10}
+              CertificationBot1={BABangaloreCourseData[0].CertificationBot1}
+              src={BABangaloreCourseData[0].src}
+              src22={BABangaloreCourseData[0].src22}
+              src33={BABangaloreCourseData[0].src33}
+              ModuleBot1={BABangaloreCourseData[0].ModuleBot1}
+              ModuleBot2={BABangaloreCourseData[0].ModuleBot2}
+              syllabush1={BABangaloreCourseData[0].syllabush1}
+              syllabusb1={BABangaloreCourseData[0].syllabusb1}
+              SyllabusBotlist11={BABangaloreCourseData[0].SyllabusBotlist11}
+              SyllabusBotlist12={BABangaloreCourseData[0].SyllabusBotlist12}
+              SyllabusBotlist13={BABangaloreCourseData[0].SyllabusBotlist13}
+              SyllabusBotlist14={BABangaloreCourseData[0].SyllabusBotlist14}
+              SyllabusBotlist15={BABangaloreCourseData[0].SyllabusBotlist15}
+              SyllabusBotlist16={BABangaloreCourseData[0].SyllabusBotlist16}
+              syllabush2={BABangaloreCourseData[0].syllabush2}
+              syllabusb2={BABangaloreCourseData[0].syllabusb2}
+              SyllabusBotlist21={BABangaloreCourseData[0].SyllabusBotlist21}
+              SyllabusBotlist22={BABangaloreCourseData[0].SyllabusBotlist22}
+              SyllabusBotlist23={BABangaloreCourseData[0].SyllabusBotlist23}
+              SyllabusBotlist24={BABangaloreCourseData[0].SyllabusBotlist24}
+              SyllabusBotlist25={BABangaloreCourseData[0].SyllabusBotlist25}
+              SyllabusBotlist26={BABangaloreCourseData[0].SyllabusBotlist26}
+              syllabush3={BABangaloreCourseData[0].syllabush3}
+              syllabusb3={BABangaloreCourseData[0].syllabusb3}
+              SyllabusBotlist31={BABangaloreCourseData[0].SyllabusBotlist31}
+              SyllabusBotlist32={BABangaloreCourseData[0].SyllabusBotlist32}
+              SyllabusBotlist33={BABangaloreCourseData[0].SyllabusBotlist33}
+              SyllabusBotlist34={BABangaloreCourseData[0].SyllabusBotlist34}
+              SyllabusBotlist35={BABangaloreCourseData[0].SyllabusBotlist35}
+              SyllabusBotlist36={BABangaloreCourseData[0].SyllabusBotlist36}
+              syllabush4={BABangaloreCourseData[0].syllabush4}
+              syllabusb4={BABangaloreCourseData[0].syllabusb4}
+              SyllabusBotlist41={BABangaloreCourseData[0].SyllabusBotlist41}
+              SyllabusBotlist42={BABangaloreCourseData[0].SyllabusBotlist42}
+              SyllabusBotlist43={BABangaloreCourseData[0].SyllabusBotlist43}
+              SyllabusBotlist44={BABangaloreCourseData[0].SyllabusBotlist44}
+              SyllabusBotlist45={BABangaloreCourseData[0].SyllabusBotlist45}
+              SyllabusBotlist46={BABangaloreCourseData[0].SyllabusBotlist46}
+              syllabussrc={BABangaloreCourseData[0].syllabussrc}
+              JobBot1={BABangaloreCourseData[0].JobBot1}
+              JobHead2={BABangaloreCourseData[0].JobHead2}
+              JobBot3={BABangaloreCourseData[0].JobBot3}
+              JobBot4={BABangaloreCourseData[0].JobBot4}
+              ProjectsBot1={BABangaloreCourseData[0].ProgramBot1}
+              src1={BABangaloreCourseData[0].src1}
+              ProjectsH1={BABangaloreCourseData[0].ProjectsH1}
+              Projectsp1={BABangaloreCourseData[0].Projectsp1}
+              src2={BABangaloreCourseData[0].src2}
+              ProjectsH2={BABangaloreCourseData[0].ProjectsH2}
+              Projectsp2={BABangaloreCourseData[0].Projectsp2}
+              src3={BABangaloreCourseData[0].src3}
+              ProjectsH3={BABangaloreCourseData[0].ProjectsH3}
+              Projectsp3={BABangaloreCourseData[0].Projectsp3}
+              src4={BABangaloreCourseData[0].src4}
+              ProjectsH4={BABangaloreCourseData[0].ProjectsH4}
+              Projectsp4={BABangaloreCourseData[0].Projectsp4}
+              AlumniBot1={BABangaloreCourseData[0].AlumniBot1}
+              Asrc1={BABangaloreCourseData[0].Asrc1}
+              AlumniH1={BABangaloreCourseData[0].AlumniH1}
+              Alumnip1={BABangaloreCourseData[0].Alumnip1}
+              Asrc2={BABangaloreCourseData[0].Asrc2}
+              AlumniH2={BABangaloreCourseData[0].AlumniH2}
+              Alumnip2={BABangaloreCourseData[0].Alumnip2}
+              Asrc3={BABangaloreCourseData[0].Asrc3}
+              AlumniH3={BABangaloreCourseData[0].AlumniH3}
+              Alumnip3={BABangaloreCourseData[0].Alumnip3}
+              Asrc4={BABangaloreCourseData[0].Asrc4}
+              AlumniH4={BABangaloreCourseData[0].AlumniH4}
+              Alumnip4={BABangaloreCourseData[0].Alumnip4}
+              alt1={BABangaloreCourseData[0].alt1}
+              alt2={BABangaloreCourseData[0].alt2}
+              alt3={BABangaloreCourseData[0].alt3}
+              alt4={BABangaloreCourseData[0].alt4}
+              alt5={BABangaloreCourseData[0].alt5}
+              alt6={BABangaloreCourseData[0].alt6}
+              alt7={BABangaloreCourseData[0].alt7}
+              alt8={BABangaloreCourseData[0].alt8}
+              alt9={BABangaloreCourseData[0].alt9}
             />
             <div className={styles.faqCity}>
-              <FAQNew FAQNewData={DataScienceCourseData[0].faq} />
+              <FAQNew FAQNewData={BABangaloreCourseData[0].faq} />
             </div>
           </div>
         </div>
         <SeventhSection />
         <CityText
-          CityTextB={DataScienceCourseData[0].CityTextB}
-          CityText={DataScienceCourseData[0].CityText}
-          CityTextLB={DataScienceCourseData[0].CityTextLB}
-          CityTextL={DataScienceCourseData[0].CityTextL}
+          CityTextB={BABangaloreCourseData[0].CityTextB}
+          CityText={BABangaloreCourseData[0].CityText}
+          CityTextLB={BABangaloreCourseData[0].CityTextLB}
+          CityTextL={BABangaloreCourseData[0].CityTextL}
         />
         <Footer />
       </main>
