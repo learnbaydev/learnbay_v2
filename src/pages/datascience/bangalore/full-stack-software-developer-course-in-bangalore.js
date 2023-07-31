9f93c964c1b9287e39f74ac601cd0115d6100223
Divyasingh88218 <divya.singh@skillslash.com>
2023-07-31 15:27:32 +0530

    dubai n bangalore fs pages

---
+++ src/pages/datascience/bangalore/full-stack-software-developer-course-in-bangalore.js
@@ -0,0 +1,318 @@
+import Head from "next/head";
+import styles from "../../../styles/Home.module.css";
+import dynamic from "next/dynamic";
+const FirstSection = dynamic(() =>
+  import("../../../../components/CoursePage/FirstSection/FirstSection")
+);
+const SecondSection = dynamic(() =>
+  import("../../../../components/CoursePage/SecondSection/SecondSection")
+);
+const ProgramInfo = dynamic(() =>
+  import("../../../../components/SEO/ProgramInfo/ProgramInfo")
+);
+const SeventhSection = dynamic(() =>
+  import("../../../../components/Global/SeventhSection/SeventhSection")
+);
+const ProgramFee = dynamic(() =>
+  import("../../../../components/SEO/ProgramInfo/ProgramFee/ProgramFee")
+);
+const CityText = dynamic(() =>
+  import("../../../../components/SEO/CityText/CityText")
+);
+const CitiesLeft = dynamic(() =>
+  import("../../../../components/SEO/CitiesLeft/CitiesLeft")
+);
+const CitiesRight = dynamic(() =>
+  import("../../../../components/SEO/CitiesRight/CitiesRight")
+);
+import { FullStackBangaloreCourseData } from "../../../../CityData/Bangalore/fullstackSoftwareDeveloperCourseInBangalore";
+const FAQNew = dynamic(() =>
+  import("../../../../components/CoursePage/FAQNew/FAQNew")
+);
+import Popup from "../../../../components/Popup/Popup";
+import Navbar from "../../../../components/Navbar/Navbar";
+import Footer from "../../../../components/Footer/Footer";
+import Form from "../../../../components/Form/Form";
+import React, { useState } from "react";
+import Testimonial from "../../../../components/HomePage/Testimonial/Testimonial";
+import FeeSection from "../../../../components/CoursePage/FeeSection/FeeSection";
+
+export default function Home() {
+  const [popups, setPopups] = useState(false);
+
+  const popupShow = () => {
+    setPopups(true);
+  };
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Full Stack Developer Course in Bangalore - Learnbay</title>
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="description"
+          content="Want to become a skilled Web Developer? Enroll Now - Learnbay's Most Extensive Full Stack Web Developer Course in Bangalore."
+        />
+        <meta
+          name="keywords"
+          content="Full-Stack Software Developer Course in Bangalore, Full-Stack Software Developer Course training in Bangalore, Full-Stack Software Developer Course institute in Bangalore, best Full-Stack Software Developer Course institute in Bangalore, data scientist course in Bangalore,data scientist certification in Bangalore, Full-Stack Software Developer Course training institute in Bangalore, advanced Full-Stack Software Developer Course in Bangalore, Full-Stack Software Developer Course with Placement Assistance, Full-Stack Software Developer Course"
+        />
+        <link
+          rel="canonical"
+          href="https://www.learnbay.co/full-stack-software-developer-course-in-bangalore"
+        />
+        <meta
+          name="copyright"
+          content="Learnbay: Full-Stack Software Developer Courses Certification Training India"
+        />
+        <link
+          rel="icon"
+          href="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/Learnbay-Favicon-L.png"
+        />
+      </Head>
+      <main>
+        {" "}
+        <Navbar popup={true} dataScience={true} />
+        <Popup trigger={popups} setTrigger={setPopups} className="popupModal">
+          <div className="leftPopup">
+            <div className="whiteP" />
+          </div>
+          <div className="RightPopup">
+            <h5>Apply For Counselling</h5>
+            <Form popup={true} setTrigger={setPopups} dataScience={true} />
+          </div>
+        </Popup>
+        <FirstSection
+          FirstTyped="Guaranteed Interview Calls"
+          SecondTyped="Capstone Project Certificate"
+          ThirdTyped="Live Interactive Classes"
+          cityParaCont=" Enroll in the best Job-ready full-stack software development course in Bangalore. Take a 10-step step ahead of others towards the most lucrative salary package of product-based MNCs, which you deserve."
+          titleCourse="Full-Stack Software Developer Course In Bangalore To Achieve Your Dream Job"
+          brochureLink="https://brochureslearnbay.s3.ap-south-1.amazonaws.com/learnbay/Full+Stack+Software+Development+Program+Learnbay.pdf"
+          FirstRightImg="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/head/W+Dev.png"
+          firstHeading="Full-Stack Software Developer Course In Bangalore To Achieve Your Dream Job"
+          firstTopPara="Ace Your Coding Interview at PRODUCT Based Companies"
+          idss="bfl64ANfSV0"
+        />
+        <SecondSection
+          SecondSectionData={FullStackBangaloreCourseData[0].secondSection}
+        />
+        <Testimonial
+          redirectDS={true}
+          Testimonial={FullStackBangaloreCourseData[0].testimonial}
+          heading="Our Alumni Speak"
+          para="Discover the impact of our programs on career growth"
+        />
+        <div className={styles.cityFee}>
+          <FeeSection
+            AIMLCertificate={true}
+            Fee="₹ 1,10,000 + 18% GST"
+            FeeEmi="₹10,816"
+            FeeHeading="Program Fee & Financing"
+            FeeContent2="No cost EMI"
+            FeeContent3="Flexible payment"
+            FeeContent4="Easy loan procedure"
+            FeeContent5="15 days refund policy"
+            dataScienceCounselling={true}
+          />
+        </div>
+        <div className="MainCities">
+          <div className="CitiesLeft">
+            <CitiesLeft />
+          </div>
+          <div className="CitiesRight">
+            <CitiesRight
+              fullStackCity={true}
+              ProgramBot1={FullStackBangaloreCourseData[0].ProgramBot1}
+              ProgramBot2={FullStackBangaloreCourseData[0].ProgramBot2}
+              ProgramBot3={FullStackBangaloreCourseData[0].ProgramBot3}
+              ProgramHead4={FullStackBangaloreCourseData[0].ProgramHead4}
+              ProgramBot4={FullStackBangaloreCourseData[0].ProgramBot4}
+              ProgramHead5={FullStackBangaloreCourseData[0].ProgramHead5}
+              ProgramBot5={FullStackBangaloreCourseData[0].ProgramBot5}
+              ProgramHead6={FullStackBangaloreCourseData[0].ProgramHead6}
+              ProgramBot6={FullStackBangaloreCourseData[0].ProgramBot6}
+              ProgramHead7={FullStackBangaloreCourseData[0].ProgramHead7}
+              ProgramBot8={FullStackBangaloreCourseData[0].ProgramBot8}
+              ProgramHead8={FullStackBangaloreCourseData[0].ProgramHead8}
+              ProgramHead9={FullStackBangaloreCourseData[0].ProgramHead9}
+              ProgramHead10={FullStackBangaloreCourseData[0].ProgramHead10}
+              ProgramHead11={FullStackBangaloreCourseData[0].ProgramHead11}
+              ProgramHead12={FullStackBangaloreCourseData[0].ProgramHead12}
+              ProgramHead13={FullStackBangaloreCourseData[0].ProgramHead13}
+              ProgramBot9={FullStackBangaloreCourseData[0].ProgramBot9}
+              DomainHead1={FullStackBangaloreCourseData[0].DomainHead1}
+              DomainBot1={FullStackBangaloreCourseData[0].DomainBot1}
+              DomainHead2={FullStackBangaloreCourseData[0].DomainHead2}
+              DomainHead3={FullStackBangaloreCourseData[0].DomainHead3}
+              DomainHead4={FullStackBangaloreCourseData[0].DomainHead4}
+              DomainHead5={FullStackBangaloreCourseData[0].DomainHead5}
+              DomainHead6={FullStackBangaloreCourseData[0].DomainHead6}
+              DomainHead7={FullStackBangaloreCourseData[0].DomainHead7}
+              DomainBot2={FullStackBangaloreCourseData[0].DomainBot2}
+              DomainBot3={FullStackBangaloreCourseData[0].DomainBot3}
+              DomainList1={FullStackBangaloreCourseData[0].DomainList1}
+              DomainList2={FullStackBangaloreCourseData[0].DomainList2}
+              DomainList3={FullStackBangaloreCourseData[0].DomainList3}
+              DomainList4={FullStackBangaloreCourseData[0].DomainList4}
+              DomainList5={FullStackBangaloreCourseData[0].DomainList5}
+              DomainList6={FullStackBangaloreCourseData[0].DomainList6}
+              DomainList7={FullStackBangaloreCourseData[0].DomainList7}
+              DomainList8={FullStackBangaloreCourseData[0].DomainList8}
+              DomainList9={FullStackBangaloreCourseData[0].DomainList9}
+              DomainList10={FullStackBangaloreCourseData[0].DomainList10}
+              Domainlink1={FullStackBangaloreCourseData[0].Domainlink1}
+              Domainlink2={FullStackBangaloreCourseData[0].Domainlink2}
+              Domainlink3={FullStackBangaloreCourseData[0].Domainlink3}
+              Domainlink4={FullStackBangaloreCourseData[0].Domainlink4}
+              Domainlink5={FullStackBangaloreCourseData[0].Domainlink5}
+              Domainlink6={FullStackBangaloreCourseData[0].Domainlink6}
+              Domainlink7={FullStackBangaloreCourseData[0].Domainlink7}
+              Domainlink8={FullStackBangaloreCourseData[0].Domainlink8}
+              Domainlink9={FullStackBangaloreCourseData[0].Domainlink9}
+              Domainlink10={FullStackBangaloreCourseData[0].Domainlink10}
+              CertificationHead1={
+                FullStackBangaloreCourseData[0].CertificationHead1
+              }
+              CertificationBot1={
+                FullStackBangaloreCourseData[0].CertificationBot1
+              }
+              src={FullStackBangaloreCourseData[0].src}
+              ModuleHead1={FullStackBangaloreCourseData[0].ModuleHead1}
+              ModuleBot1={FullStackBangaloreCourseData[0].ModuleBot1}
+              ModuleHead2={FullStackBangaloreCourseData[0].Head2}
+              ModuleBot3={FullStackBangaloreCourseData[0].ModuleBot3}
+              ModuleBot2={FullStackBangaloreCourseData[0].ModuleBot2}
+              syllabush1={FullStackBangaloreCourseData[0].syllabush1}
+              syllabusb1={FullStackBangaloreCourseData[0].syllabusb1}
+              SyllabusBotlist11={
+                FullStackBangaloreCourseData[0].SyllabusBotlist11
+              }
+              SyllabusBotlist12={
+                FullStackBangaloreCourseData[0].SyllabusBotlist12
+              }
+              SyllabusBotlist13={
+                FullStackBangaloreCourseData[0].SyllabusBotlist13
+              }
+              SyllabusBotlist14={
+                FullStackBangaloreCourseData[0].SyllabusBotlist14
+              }
+              SyllabusBotlist15={
+                FullStackBangaloreCourseData[0].SyllabusBotlist15
+              }
+              SyllabusBotlist16={
+                FullStackBangaloreCourseData[0].SyllabusBotlist16
+              }
+              syllabush2={FullStackBangaloreCourseData[0].syllabush2}
+              syllabusb2={FullStackBangaloreCourseData[0].syllabusb2}
+              SyllabusBotlist21={
+                FullStackBangaloreCourseData[0].SyllabusBotlist21
+              }
+              SyllabusBotlist22={
+                FullStackBangaloreCourseData[0].SyllabusBotlist22
+              }
+              SyllabusBotlist23={
+                FullStackBangaloreCourseData[0].SyllabusBotlist23
+              }
+              SyllabusBotlist24={
+                FullStackBangaloreCourseData[0].SyllabusBotlist24
+              }
+              SyllabusBotlist25={
+                FullStackBangaloreCourseData[0].SyllabusBotlist25
+              }
+              SyllabusBotlist26={
+                FullStackBangaloreCourseData[0].SyllabusBotlist26
+              }
+              syllabush3={FullStackBangaloreCourseData[0].syllabush3}
+              syllabusb3={FullStackBangaloreCourseData[0].syllabusb3}
+              SyllabusBotlist31={
+                FullStackBangaloreCourseData[0].SyllabusBotlist31
+              }
+              SyllabusBotlist32={
+                FullStackBangaloreCourseData[0].SyllabusBotlist32
+              }
+              SyllabusBotlist33={
+                FullStackBangaloreCourseData[0].SyllabusBotlist33
+              }
+              SyllabusBotlist34={
+                FullStackBangaloreCourseData[0].SyllabusBotlist34
+              }
+              SyllabusBotlist35={
+                FullStackBangaloreCourseData[0].SyllabusBotlist35
+              }
+              SyllabusBotlist36={
+                FullStackBangaloreCourseData[0].SyllabusBotlist36
+              }
+              syllabush4={FullStackBangaloreCourseData[0].syllabush4}
+              syllabusb4={FullStackBangaloreCourseData[0].syllabusb4}
+              SyllabusBotlist41={
+                FullStackBangaloreCourseData[0].SyllabusBotlist41
+              }
+              SyllabusBotlist42={
+                FullStackBangaloreCourseData[0].SyllabusBotlist42
+              }
+              SyllabusBotlist43={
+                FullStackBangaloreCourseData[0].SyllabusBotlist43
+              }
+              SyllabusBotlist44={
+                FullStackBangaloreCourseData[0].SyllabusBotlist44
+              }
+              SyllabusBotlist45={
+                FullStackBangaloreCourseData[0].SyllabusBotlist45
+              }
+              SyllabusBotlist46={
+                FullStackBangaloreCourseData[0].SyllabusBotlist46
+              }
+              syllabussrc={FullStackBangaloreCourseData[0].syllabussrc}
+              JobHead1={FullStackBangaloreCourseData[0].JobHead1}
+              JobBot1={FullStackBangaloreCourseData[0].JobBot1}
+              JobBot2={FullStackBangaloreCourseData[0].JobBot2}
+              JobBot5={FullStackBangaloreCourseData[0].JobBot5}
+              JobHead2={FullStackBangaloreCourseData[0].JobHead2}
+              JobBot3={FullStackBangaloreCourseData[0].JobBot3}
+              JobBot4={FullStackBangaloreCourseData[0].JobBot4}
+              JobBot6={FullStackBangaloreCourseData[0].JobBot6}
+              ProjectsBot1={FullStackBangaloreCourseData[0].ProgramBot1}
+              ProjectsBot2={FullStackBangaloreCourseData[0].ProgramBot2}
+              src1={FullStackBangaloreCourseData[0].src1}
+              ProjectsH1={FullStackBangaloreCourseData[0].ProjectsH1}
+              Projectsp1={FullStackBangaloreCourseData[0].Projectsp1}
+              src2={FullStackBangaloreCourseData[0].src2}
+              ProjectsH2={FullStackBangaloreCourseData[0].ProjectsH2}
+              Projectsp2={FullStackBangaloreCourseData[0].Projectsp2}
+              src3={FullStackBangaloreCourseData[0].src3}
+              ProjectsH3={FullStackBangaloreCourseData[0].ProjectsH3}
+              Projectsp3={FullStackBangaloreCourseData[0].Projectsp3}
+              src4={FullStackBangaloreCourseData[0].src4}
+              ProjectsH4={FullStackBangaloreCourseData[0].ProjectsH4}
+              Projectsp4={FullStackBangaloreCourseData[0].Projectsp4}
+              AlumniBot1={FullStackBangaloreCourseData[0].AlumniBot1}
+              Asrc1={FullStackBangaloreCourseData[0].Asrc1}
+              AlumniH1={FullStackBangaloreCourseData[0].AlumniH1}
+              Alumnip1={FullStackBangaloreCourseData[0].Alumnip1}
+              Asrc2={FullStackBangaloreCourseData[0].Asrc2}
+              AlumniH2={FullStackBangaloreCourseData[0].AlumniH2}
+              Alumnip2={FullStackBangaloreCourseData[0].Alumnip2}
+              Asrc3={FullStackBangaloreCourseData[0].Asrc3}
+              AlumniH3={FullStackBangaloreCourseData[0].AlumniH3}
+              Alumnip3={FullStackBangaloreCourseData[0].Alumnip3}
+              Asrc4={FullStackBangaloreCourseData[0].Asrc4}
+              AlumniH4={FullStackBangaloreCourseData[0].AlumniH4}
+              Alumnip4={FullStackBangaloreCourseData[0].Alumnip4}
+            />
+            <div className={styles.faqCity}>
+              <FAQNew FAQNewData={FullStackBangaloreCourseData[0].faq} />
+            </div>
+          </div>
+        </div>
+        <SeventhSection />
+        <CityText
+          CityTextB={FullStackBangaloreCourseData[0].CityTextB}
+          CityText={FullStackBangaloreCourseData[0].CityText}
+          CityTextLB={FullStackBangaloreCourseData[0].CityTextLB}
+          CityTextL={FullStackBangaloreCourseData[0].CityTextL}
+        />
+        <Footer />
+      </main>
+    </div>
+  );
+}
