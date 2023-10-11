61313a7489c592f4b5bf5cbb44477df2e6240b8b
AmitAmbi <amit@learnbay.co>
2023-10-11 14:30:06 +0530

    thirdsection contact

--- components/MastersCourse/ContactCounsellor/ContactCounsellor.js
+++ components/MastersCourse/ContactCounsellor/ContactCounsellor.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../ContactCounsellor/ContactCounsellor.module.css'
 import {MdOutlineWhatsapp} from 'react-icons/md'
+import {AiOutlineMail} from 'react-icons/ai'
 
 function ContactCounsellor() {
   return (
@@ -9,10 +10,12 @@ function ContactCounsellor() {
 <h4>Can’t decide which program to opt for?</h4>
 <p>Don’t worry, our expert counsellor is there to guide you make the right career choice</p>
 <div className={styles.conbtn}>
-<button onClick={null}> (+91) 77956 87988</button>
-<button onClick={true}> contacts@learnbay.co</button>
+<button onClick={null}> <MdOutlineWhatsapp className={styles.conicon}/>(+91) 77956 87988</button>
+<button onClick={true}><AiOutlineMail className={styles.conicon}/> contacts@learnbay.co</button>
 </div>
+<div className={styles.consbtn1}>
 <button className={styles.consbtn} onClick={null}>Apply for counselling</button>
+</div>
       </div>
 
     
