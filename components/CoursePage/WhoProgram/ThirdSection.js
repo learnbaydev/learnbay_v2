e0123fea674095d8049a1f75835260cbd0cdd772
app[le <apple@apples-MacBook-Pro.local>
2023-10-30 02:12:17 -0700

    third sec

--- components/CoursePage/WhoProgram/ThirdSection.js
+++ components/CoursePage/WhoProgram/ThirdSection.js
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./ThirdSection.module.css";
 import Image from "next/image";
+import Button from "../../Global/Button/Button";
+import { FaCheckCircle, FaDownload } from "react-icons/fa";
+
 
 function ThirdSection({ leftImage, ThirdSectionData }) {
   return (
     <div className={`${styles.program} wrapper`} id="eligibility">
-      <h4>Who is this program for?</h4>
+      <h4>Choose Your Domain Elective</h4>
       <div className={styles.programWrap}>
         <div className={styles.left}>
           <Image src={leftImage} width={500} height={514} alt="who" />
@@ -17,10 +20,23 @@ function ThirdSection({ leftImage, ThirdSectionData }) {
               return (
                 <div className={styles.leftSide} key={index}>
                   <div className={styles.boxIcon} >
-                    <div>{icon}</div>
+                    <div> <Image
+                      src={icon}
+                      loading="lazy"
+                      width={40}
+                      height={40}
+                    /></div>
                   </div>
                   <h5 className={styles.heading}>{heading}</h5>
                   <p>{para}</p>
+
+                  <div className={styles.buttonDiv}>
+                  <Button
+                
+                    text="Brochure"
+                    passIcon={<FaDownload className="bIconS" />}
+                  />
+                </div>
                 </div>
               );
             })}
