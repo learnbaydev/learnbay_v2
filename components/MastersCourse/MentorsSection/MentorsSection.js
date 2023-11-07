00ae5fbdac144f99de6f501963d7a4dbfe262f2d
app[le <apple@apples-MacBook-Pro.local>
2023-11-07 19:42:11 +0530

    mentor sec

--- components/MastersCourse/MentorsSection/MentorsSection.js
+++ components/MastersCourse/MentorsSection/MentorsSection.js
@@ -41,19 +41,33 @@ function MentorsSection() {
       setValue(4);
     }
   }, []);
+
+  const mentorImg = [
+    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/mentor-desktop.webp",
+    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/puulkit.webp",
+    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/tripti.webp",
+    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/AnkurK.webp",
+  ];
+
+  const companyImg = [
+    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/kryndal.webp",
+    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/paypal.webp",
+    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/paytm.webp",
+    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/walmart.webp",
+  ];
+
+  const mentorNames = ["Mohit S.", "Pulkit Jain", "Tripti Jain", "Ankur K."];
+
+
   return (
     <section className={styles.container}>
       <h4>Meet Our Mentors</h4>
-      <p>Guiding You Every Step of the WayTowards Professional Excellence</p>
+      <p>Guiding You Every Step of the Way Towards Professional Excellence</p>
       <div className={styles.SliderDiv}>
         <Swiper
           loop={true}
           slidesPerView={value}
-          spaceBetween={"20px"}
-          
-           
-            
-            
+          spaceBetween={20}
           pagination={{
             dynamicBullets: true,
             clickable: true,
@@ -63,119 +77,34 @@ function MentorsSection() {
           modules={[Pagination, Navigation, Scrollbar]}
           className="mySwiper"
         >
-              <SwiperSlide className={styles.slide}>
-                <div className={styles.box}>
-                  <div>
-                    <Image
-                      src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/mentor-desktop.webp"
-                      width="100"
-                      height="100"
-                      //  
-                      alt="data science course"
-                      //  quality={80}
-                      // loading="lazy"
-
-                    />
-                  </div>
-                  <div>
-                    <p className={styles.Name}>Mohit S.</p>
-                    <Image
-                      src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/kryndal.webp"
-                      width="100"
-                      height="25"
-                       
-                      alt="data science course"
-                       quality={80}
-                      loading="lazy"
-                    />
-                  </div>
-                  {/* <p className={styles.cont}>Elevate Your Profile: Boost your career prospects </p> */}
+          {mentorImg.map((mentor, index) => (
+            <SwiperSlide key={`mentor-${index}`} className={styles.slide}>
+              <div className={styles.box}>
+                <div>
+                  <Image
+                    src={mentor}
+                    width={100}
+                    height={100}
+                    alt="mentor"
+                    quality={80}
+                    loading="lazy"
+                  />
                 </div>
-              </SwiperSlide>
-              <SwiperSlide className={styles.slide}>
-                <div className={styles.box}>
-                  <div>
-                    <Image
-                      src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/puulkit.webp"
-                      width="100"
-                      height="100"
-                       
-                      alt="data science course"
-                       quality={80}
-                      loading="lazy"
-                    />
-                  </div>
-                  <div>
-                    <p className={styles.Name}>Pulkit Jain</p>
-                    <Image
-                      src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/paypal.webp"
-                      width="100"
-                      height="30"
-                       
-                      alt="data science course"
-                       quality={80}
-                      loading="lazy"
-                    />
-                  </div>
-                  {/* <p className={styles.cont}>Elevate Your Profile: Boost your career prospects </p> */}
+                <div>
+                  <p className={styles.Name}>{mentorNames[index]}</p>
+                  <Image
+                    src={companyImg[index]}
+                    width={100}
+                    height={25}
+                    alt="company"
+                    quality={80}
+                    loading="lazy"
+                  />
+                
                 </div>
-              </SwiperSlide>
-              <SwiperSlide className={styles.slide}>
-                <div className={styles.box}>
-                  <div>
-                    <Image
-                      src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/tripti.webp"
-                      width="100"
-                      height="100"
-                       
-                      alt="data science course"
-                       quality={80}
-                      loading="lazy"
-                    />
-                  </div>
-                  <div>
-                    <p className={styles.Name}>Tripti Jain</p>
-                    <Image
-                      src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/paytm.webp"
-                      width="100"
-                      height="30"
-                       
-                      alt="data science course"
-                       quality={80}
-                      loading="lazy"
-                    />
-                  </div>
-                  {/* <p className={styles.cont}>Elevate Your Profile: Boost your career prospects </p> */}
-                </div>
-              </SwiperSlide>
-              <SwiperSlide className={styles.slide}>
-                <div className={styles.box}>
-                  <div>
-                    <Image
-                      src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/AnkurK.webp"
-                      width="100"
-                      height="100"
-                       
-                      alt="data science course"
-                       quality={80}
-                      loading="lazy"
-                    />
-                  </div>
-                  <div>
-                    <p className={styles.Name}>Ankur K.</p>
-                    <Image
-                      src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/walmart.webp"
-                      width="100"
-                      height="20"
-                       
-                      alt="data science course"
-                       quality={80}
-                      loading="lazy"
-                    />
-                  </div>
-                  {/* <p className={styles.cont}>Elevate Your Profile: Boost your career prospects </p> */}
-                </div>
-              </SwiperSlide>
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </section>
