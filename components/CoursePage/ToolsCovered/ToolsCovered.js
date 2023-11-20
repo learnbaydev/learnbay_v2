883cfde473bfbe55602119857cd3510023dbfd1e
app[le <apple@apples-MacBook-Pro.local>
2023-11-20 17:29:16 +0530

    test

--- components/CoursePage/ToolsCovered/ToolsCovered.js
+++ components/CoursePage/ToolsCovered/ToolsCovered.js
@@ -1,5 +1,4 @@
 
-
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper";
@@ -8,64 +7,27 @@ import styles from "./ToolsCovered.module.css";
 import Image from "next/image";
 const ToolsCovered = () => {
   // Sample image URLs, replace these with your own image URLs
-  const images = [
-    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/masterPage/tool1.webp",
-    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/masterPage/tool2.webp",
-    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/masterPage/tool3.webp",
-    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/masterPage/tool4.webp",
-    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/masterPage/tool5.webp",
-    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/masterPage/tool1.webp",
-    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/masterPage/tool2.webp",
-    "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/masterPage/tool3.webp",
-  ];
+
   return (
     <>
-      <p className={styles.infop}>Programming Languages & Tools Covered</p>
-      <div className={styles.sliderContainer}>
-        <Swiper
-        loop={true}
-        autoplay={{
-          delay: 1500,
-          disableOnInteraction: false,
-        }}
-          pagination={{
-            dynamicBullets: true,
-            clickable: true,
-          }}
-          grabCursor={true}
-          breakpoints={{
-            1280: { slidesPerView: 5, spaceBetween: 30 },
-            481: {
-              slidesPerView: 3,
-              spaceBetween: 10,
-            },
-            393: {
-              slidesPerView: 3,
-              spaceBetween: 10,
-            },
-          }}
-          modules={[ Pagination, Autoplay]}
-          className="mySwiper"
-         
-        >
-          {images.map((image, index) => (
-            <SwiperSlide key={index} className={styles.margin}>
-              <div className={`${styles.column} imgWrapper`}>
-                <Image
-                  src={image}
-                 
-                  width={80}
-                  height={60}
-                  loading="lazy"
-                  quality={60}
-                 
-                />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+    <p className={styles.infop}>Programming Languages & Tools Covered</p>
+    <div className={styles.container}>
+
+      <div className="imgWrapper">
+
+        <Image
+          src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/learnbayMain/toolscover.webp"
+          width="800"
+          height="800"
+          loading="lazy"
+          alt="data science course"
+        />
       </div>
+
+    </div>
     </>
+
+
   );
 };
 export default ToolsCovered;
